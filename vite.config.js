b1c65c3a06ffa3dc8627f042ffadcc7077471b6a
Daniel Palma <danielpalma2003@hotmail.com>
2024-04-28 22:56:55 -0500

    Chore: Updated build config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "./build",
+    assetsDir: "./assets",
+    publicDir: "./public"
+  },
   test: {
     globals: true,
     environment: 'jsdom',
