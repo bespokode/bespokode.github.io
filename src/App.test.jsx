4f25c2590d152e3910716e4f617b40569566514f
Daniel Palma <danielpalma2003@hotmail.com>
2024-04-28 23:16:33 -0500

    fix: updated tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,27 +1,14 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './Def';
 
-test('renders counter button', () => {
+test('renders home react link', () => {
   render(<App />);
-  const buttonElement = screen.getByText(/count is 0/i);
-  expect(buttonElement).toBeInTheDocument();
-});
-
-test('count increases on button click', () => {
-  render(<App />);
-  const buttonElement = screen.getByText(/count is 0/i);
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent(/count is 1/i);
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders vite docs link', () => {
+test('renders info link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/vite docs/i);
+  const linkElement = screen.getByText(/Nosotros/i);
   expect(linkElement).toBeInTheDocument();
 });
