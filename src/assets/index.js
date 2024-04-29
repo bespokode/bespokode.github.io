b6c45ac1755bf1d57af07b401ce46a2b417963ee
Daniel Palma <danielpalma2003@hotmail.com>
2024-04-29 14:34:46 -0500

    Fix: updated file extension to typescript file and add typos

--- src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import imagotipo_s from "./icons/imagotipo-s-dark.png";
-import bg_index_image from './index/background-index-image.png';
-
-export {
-    imagotipo_s,
-    bg_index_image
-}
---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import imagotipo_s_dark from "./icons/imagotipo-s-dark.png";
+import imagotipo_s_light from "./icons/imagotipo-s-light.png";
+import bg_index_image from './index/background-index-image.png';
+
+export {
+    imagotipo_s_dark,
+    imagotipo_s_light,
+    bg_index_image
+}
