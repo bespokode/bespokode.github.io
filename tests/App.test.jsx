4d668b42c559f39006eb203bd3dce04b760f3d60
Daniel Palma <danielpalma2003@hotmail.com>
2024-04-29 07:50:14 -0500

    Chore: moved tests to separated folder

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,13 +1,13 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './Def';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "../src/App";
 
-test('renders home react link', () => {
+test("renders home react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders info link', () => {
+test("renders info link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Nosotros/i);
   expect(linkElement).toBeInTheDocument();
