fdd21b7c8c677bf509b9e8676333da7bf1df79e4
Daniel Palma <danielpalma2003@hotmail.com>
2024-04-28 23:02:14 -0500

    fix: updated conflictive filename

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from './Def';
 
 test('renders counter button', () => {
   render(<App />);
---
+++ src/Def.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+import CustomNavBar from "./components/CustomNavbar";
+import React from "react";
+import Home from "./pages/Home";
+import Info from "./pages/Info";
+
+function App() {
+  return (
+    <HashRouter>
+      <CustomNavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+export default App;
