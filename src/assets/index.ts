428614b2217d968a9ead26cf21480a06c854d6c0
Daniel Palma <danielpalma2003@hotmail.com>
2024-05-02 00:56:47 -0500

    onprogress: Updated landing page
    New captions card, new typewriting effect and fixed css errors

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,13 @@
+//* imagotipo
 import imagotipo_s_dark from "./icons/imagotipo-s-dark.png";
 import imagotipo_s_light from "./icons/imagotipo-s-light.png";
+//* backgrounds
 import bg_index_image from './index/background-index-image.png';
+import bg_laptop_image from './backgrounds/laptop.jpg';
 
 export {
     imagotipo_s_dark,
     imagotipo_s_light,
-    bg_index_image
+    bg_index_image,
+    bg_laptop_image
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,37 @@
 import React from "react";
-import { Image, Stack } from "react-bootstrap";
-import { bg_index_image } from "../assets";
+import { Carousel, Image, Stack } from "react-bootstrap";
+import { bg_index_image, bg_laptop_image } from "../assets";
+import TypewriterEffect from "../components/typewriter";
+import CaptionCard from "../components/CaptionCard";
 
 const Home = () => {
   return (
-    <Stack className="w-100 h-100 ">
-      <div className="w-100 overflow-hidden h-75 d-flex justify-content-center align-items-center ">
+    <Stack className="w-100">
+      <div
+        className="w-100 overflow-hidden d-flex justify-content-center align-items-center bg-dark"
+        style={{ height: "85vh" }}
+      >
         <Image
           src={bg_index_image}
           className="opacity-25 w-100 h-100 object-fit-cover "
         />
-        <h1 className="text-center my-auto font-astro position-absolute top-50 start-50 translate-middle">
+        <h1 className="text-center font-astro position-absolute top-50 start-50 translate-middle text-light">
           BespoKode Solutions
           <br />
-          Software
+          <TypewriterEffect />
         </h1>
       </div>
-      <div>
-        <h1>hola</h1>
+      <div className="mt-5 text-center">
+        <h1 className="fw-bold">Experts in bespoke software solutions.</h1>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col m-2 rounded">
+              <CaptionCard ImgSource={bg_laptop_image} className="w-100"  />
+            </div>
+            <div className="col-6 bg-info m-2 rounded">2 of 3 (wider)</div>
+            <div className="col bg-info m-2 rounded">3 of 3</div>
+          </div>
+        </div>
       </div>
     </Stack>
   );
