f2bdc6835bb4ed9726e19bc7fc60519e2d66239b
Daniel Palma <danielpalma2003@hotmail.com>
2024-05-11 22:34:49 -0500

    feat: component updated

--- src/components/typewriter.tsx
+++ src/components/typewriter.tsx
@@ -1,38 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import useInterval from "../helpers/useInterval";
 
-const TypewriterEffect = () => {
-  const words = ["hola", "maquina", "yo"];
-  const speed = 50;
-  const [displayWordIndex, setDisplayWordIndex] = useState(0);
-  const [displayText, setDisplayText] = useState("");
+function sleep(ms: number) {
+  const start = Date.now();
+  while (Date.now() - start < ms);
+}
 
-  useEffect(() => {
-    let currentIndex = 0;
-    let intervalId = setInterval(() => {});
 
-    const typeNextCharacter = () => {
-      const currentWord = words[displayWordIndex];
-
-      if (currentIndex > currentWord.length) {
-        setDisplayText((prevText) => prevText + currentWord[currentIndex]);
-        currentIndex++;
-      } else {
-        clearInterval(intervalId);
-        setTimeout(() => {
-          setDisplayWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-          setDisplayText("");
-          currentIndex = 0;
-          intervalId = setInterval(typeNextCharacter, speed);
-        }, 1000); // Espera 1 segundo antes de comenzar la siguiente palabra
+const TypewriterEffect = ({ phrases }: { phrases: string[] }) => {
+  const [stringIndex, setStringIndex] = useState(0);
+  const [phraseIndex, setPhraseIndex] = useState(0);
+  useInterval(() => {
+    if (stringIndex < phrases[phraseIndex].length) {
+      setStringIndex((prev) => prev + 1);
+    } else {
+      sleep(800);
+      setStringIndex(0);
+      if (phraseIndex + 1 >= phrases.length) {
+        setPhraseIndex(0);
+        return;
       }
-    };
-
-    intervalId = setInterval(typeNextCharacter, speed);
-
-    return () => clearInterval(intervalId);
-  }, [words, speed, displayWordIndex]);
-
-  return <span>{displayText}</span>;
+      setPhraseIndex((prev) => prev + 1);
+    }
+  }, 200);
+  return <span>{phrases[phraseIndex].slice(0, stringIndex)}&nbsp; </span>;
 };
 
 export default TypewriterEffect;
